4be511eca2760da429cc2fcd1d4cfa92d9d1aa48
kingkill2328 <roufwarith28@gmail.com>
2025-08-01 10:32:00 +0800

    day 6 done

---
+++ day 6/ac2.py
@@ -0,0 +1,21 @@
+a = 10
+b = 12
+c = 12
+print(a!=b)#true
+print(b!=c)#false
+
+a = 'python'
+b = 'coding'
+if a!=b:
+    print("a and b are diffrent")
+else:
+    print("a and b are same")
+
+a = 4 
+b = 5
+if a == 1 != b == 5:
+    print("Hello")
+
+a = int(input("Enter a number"))
+if a%2 == 0:
+    print("even number")
